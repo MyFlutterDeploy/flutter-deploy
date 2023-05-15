36b79c9b6abd7b23a4ed88427157926af504cb83
lamartinezt <127248812+lamartinezt@users.noreply.github.com>
2023-05-15 12:21:56 -0500

    Create replace_env2.js

---
+++ .github/scripts/replace_env2.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+const filePath = 'android/app/client_secret.json';
+const GOOGLE_PRIVATE_KEY = process.env.GOOGLE_PRIVATE_KEY;
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const updatedContent = data.replace(/#{GOOGLE_PRIVATE_KEY}#/g, GOOGLE_PRIVATE_KEY);
+
+  fs.writeFile(filePath, updatedContent, 'utf8', (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    console.log('Archivo actualizado con éxito.');
+  });
+});
